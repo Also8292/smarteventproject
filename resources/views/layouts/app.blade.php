1d7c6e89d71016537b2706d08820a0a69a77c401
Also8292 <sowalils1992@gmail.com>
2018-09-25 19:43:00 +0000

    home personalized

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,20 +19,35 @@
     
     <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container-fluid">
-            <div class="navbar-header"><a class="navbar-brand navbar-link" href="#">Listing </a>
+            <div class="navbar-header"><a class="navbar-brand navbar-link" href="/">Listing </a>
                 <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
             </div>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav navbar-right">
+                @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                @else
                     <li class="" role="presentation"><a href="/listing">All Events</a></li>
                     <li role="presentation"><a href="/createEvent">Add Event</a></li>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="">Aliou <span class="caret"></span></a>
+
+                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="">{{ Auth::user()->name }} <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
                             <li role="presentation"><a href="/profile">Profile</a></li>
                             <li role="presentation"><a href="#">Second Item</a></li>
-                            <li role="presentation"><a href="#">Logout</a></li>
+                            <li role="presentation">
+                                <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
                         </ul>
                     </li>
+                @endif
                 </ul>
             </div>
         </div>
